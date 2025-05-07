750112c53abd9fe3c28f3b385f99624001cab268
Phoebe <gintarasu53@gmail.com>
2025-05-07 23:03:59 +0300

    Added short description.

--- core/src/main/java/io/github/some_example_name/PassiveItem.java
+++ core/src/main/java/io/github/some_example_name/PassiveItem.java
@@ -1,5 +1,8 @@
 package io.github.some_example_name;
 
+//Template for an item the player can find
+//that passively helps the player.
+
 public class PassiveItem extends Item
 {
     //private passive passive;
