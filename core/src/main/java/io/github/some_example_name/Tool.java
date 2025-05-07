040f7ce730d7964306a84968a9d60ce5c4b52cc7
Phoebe <gintarasu53@gmail.com>
2025-05-07 23:04:52 +0300

    Added short description.

--- core/src/main/java/io/github/some_example_name/Tool.java
+++ core/src/main/java/io/github/some_example_name/Tool.java
@@ -1,5 +1,8 @@
 package io.github.some_example_name;
 
+//Template for a weapon style item.
+//Player will start off with some basic ones.
+
 public class Tool extends Item
 {
     private Action action = new Action(1, 1);
