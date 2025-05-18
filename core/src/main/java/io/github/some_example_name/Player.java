538f5a4565873f861ce0ccc567f5b524ba058050
priincemaxx <maxx.tamos@gmail.com>
2025-05-18 23:16:20 +0300

    Troubleshooting addListener

--- core/src/main/java/io/github/some_example_name/Player.java
+++ core/src/main/java/io/github/some_example_name/Player.java
@@ -1,5 +1,16 @@
 package io.github.some_example_name;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.ApplicationAdapter;
+
+
 public class Player extends Entity
 {
     public static final int EQUIP_SLOTS = 4;
@@ -87,4 +98,50 @@ public class Player extends Entity
 
         actingTool.callAction(this, target);
     }
+    /** Creates action buttons for combat.
+     * @param root Root table to add buttons to.
+     * @param skin Skin of the buttons.
+     */
+    public void createActionButton(Table root, Skin skin, Stage stage, Player player, Enemy enemy)
+    {
+        Gdx.input.setInputProcessor(stage);
+        root.bottom().padBottom(20);
+        root.defaults().expandX().uniform().fillX().pad(20);
+
+        Tool toolButton1 = equippedTools.getTool(0);
+        Tool toolButton2 = equippedTools.getTool(1);
+        root.row();
+        Tool toolButton3 = equippedTools.getTool(2);
+        Tool toolButton4 = equippedTools.getTool(3);
+
+        toolButton1.addListener(new ChangeListener()
+        {
+            @Override
+            public void changed(ChangeEvent event, Actor actor)
+            {
+                System.out.println("Player health: " + player.getHealth());
+                System.out.println("Puny Leaf health: " + enemy.getHealth());
+                player.doMoveOn(enemy);
+                enemy.doMoveOn(player);
+            }
+        });
+
+
+
+//        for(int i=0; i<EQUIP_SLOTS; i++)
+//        {
+//            Tool currentTool = equippedTools.getTool(i);
+//            if(currentTool != null)
+//            {
+//                TextButton toolButton = new TextButton(currentTool.getName() , skin);
+//                root.add(toolButton);
+//            } else {
+//                TextButton disabledButton = new TextButton("No tool equipped", skin);
+//                root.add(disabledButton)
+//                disabledButton.setDisabled(true);
+//            }
+//            if(i == 1) {root.row(); }
+//        }
+    }
+
 }
